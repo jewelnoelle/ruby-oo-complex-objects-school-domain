dba20773acd58a3e00cf7122544ad46938d1926b
Jewel Carrier-Davis <jujicb@gmail.com>
2021-03-07 17:24:57 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+    def initialize(model)
+        @model = model
+    end
+
+    def roster=(roster)
+        @roster = roster
+    end
+
+    def roster
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] = []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        school.roster[grade]
+    end
+
+    def sort
+        @roster.map do |grade, roster|
+            @roster[grade] = roster.sort!
+        end
+        @roster
+    end
+end
+
